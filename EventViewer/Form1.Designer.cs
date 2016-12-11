62948b2f08ec8394a4b3db993278ec214626076b
imcvampire <quocanh96@gmail.com>
2016-12-11 22:00:55 +0700

    First simple version

--- EventViewer/Form1.Designer.cs
+++ EventViewer/Form1.Designer.cs
@@ -29,67 +29,98 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Applications");
-            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Security");
-            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Setup");
-            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("System");
-            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Events", new System.Windows.Forms.TreeNode[] {
-            treeNode1,
-            treeNode2,
-            treeNode3,
-            treeNode4});
-            this.eventLog1 = new System.Diagnostics.EventLog();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Application");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("System");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Security");
             this.treeView1 = new System.Windows.Forms.TreeView();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.No = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Message = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Source = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
             this.SuspendLayout();
             // 
-            // eventLog1
-            // 
-            this.eventLog1.SynchronizingObject = this;
-            // 
             // treeView1
             // 
+            this.treeView1.AccessibleDescription = "Log";
+            this.treeView1.AccessibleName = "Log";
             this.treeView1.Location = new System.Drawing.Point(20, 10);
             this.treeView1.Margin = new System.Windows.Forms.Padding(2);
             this.treeView1.Name = "treeView1";
-            treeNode1.Name = "Applications";
-            treeNode1.Text = "Applications";
-            treeNode2.Name = "Security";
-            treeNode2.Text = "Security";
-            treeNode3.Name = "Setup";
-            treeNode3.Text = "Setup";
-            treeNode4.Name = "System";
-            treeNode4.Text = "System";
-            treeNode5.Name = "Events";
-            treeNode5.Text = "Events";
+            treeNode1.Name = "Application";
+            treeNode1.Text = "Application";
+            treeNode2.Name = "System";
+            treeNode2.Text = "System";
+            treeNode3.Name = "Security";
+            treeNode3.Text = "Security";
             this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode5});
+            treeNode1,
+            treeNode2,
+            treeNode3});
             this.treeView1.Size = new System.Drawing.Size(149, 395);
             this.treeView1.TabIndex = 0;
             this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             // 
+            // eventLog1
+            // 
+            this.eventLog1.Log = global::EventViewer.Properties.Settings.Default.Application;
+            this.eventLog1.Source = global::EventViewer.Properties.Settings.Default.Application;
+            this.eventLog1.SynchronizingObject = this;
+            this.eventLog1.EntryWritten += new System.Diagnostics.EntryWrittenEventHandler(this.eventLog1_EntryWritten);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.No,
+            this.ID,
+            this.Message,
+            this.Source});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(175, 10);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(829, 395);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // No
+            // 
+            this.No.Text = "No";
+            this.No.Width = 57;
+            // 
+            // Message
+            // 
+            this.Message.Text = "Message";
+            this.Message.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Message.Width = 498;
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            this.ID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ID.Width = 86;
+            // 
+            // Source
+            // 
+            this.Source.Text = "Source";
+            this.Source.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Source.Width = 178;
+            // 
             // form1BindingSource
             // 
             this.form1BindingSource.DataSource = typeof(EventViewer.Form1);
-            this.form1BindingSource.CurrentChanged += new System.EventHandler(this.form1BindingSource_CurrentChanged);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(175, 12);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(829, 393);
-            this.textBox1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1016, 423);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.treeView1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
@@ -97,7 +128,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -106,7 +136,12 @@
         private System.Diagnostics.EventLog eventLog1;
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.BindingSource form1BindingSource;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader No;
+        private System.Windows.Forms.ColumnHeader ID;
+        private System.Windows.Forms.ColumnHeader Message;
+        private System.Windows.Forms.ColumnHeader Source;
     }
 }
 
--- EventViewer/Form1.cs
+++ EventViewer/Form1.cs
@@ -11,21 +11,39 @@ namespace EventViewer
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            eventLog1.Log = "Application";
+            eventLog1.Source = "Event Viewer";
+        }
+
         private void eventLog1_EntryWritten(object sender, EntryWrittenEventArgs e)
         {
-            eventLog1.Source = "Event Log Test 1";
+            eventLog1.Log = "Application";
+            eventLog1.Source = "Event Viewer";
 
             eventLog1.WriteEntry("Log is written.");
         }
 
         private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
-            Console.WriteLine(e.Node);
+            eventLog1.Log = e.Node.Name;
+
+            listView1.Items.Clear();
+          
+            for (int i = 0; i < eventLog1.Entries.Count; i++)
+            {
+                ListViewItem listViewItem = new ListViewItem(i.ToString());
+                listViewItem.SubItems.Add(eventLog1.Entries[i].InstanceId.ToString());
+                listViewItem.SubItems.Add(eventLog1.Entries[i].Message);
+                listViewItem.SubItems.Add(eventLog1.Entries[i].Source);
+                listView1.Items.Add(listViewItem);
+            }
         }
 
-        private void form1BindingSource_CurrentChanged(object sender, EventArgs e)
+        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
         {
-
+            
         }
     }
 }
