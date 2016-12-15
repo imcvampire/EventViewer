aece5a5d1fcb4df24199d74ab8e14644e686c4cf
imcvampire <quocanh96@gmail.com>
2016-12-15 14:45:40 +0700

    Add many things

--- EventViewer/Form1.Designer.cs
+++ EventViewer/Form1.Designer.cs
@@ -36,11 +36,29 @@
             this.eventLog1 = new System.Diagnostics.EventLog();
             this.listView1 = new System.Windows.Forms.ListView();
             this.No = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.EventID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Message = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Source = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DateAndTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,7 +66,8 @@
             // 
             this.treeView1.AccessibleDescription = "Log";
             this.treeView1.AccessibleName = "Log";
-            this.treeView1.Location = new System.Drawing.Point(20, 10);
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
             this.treeView1.Margin = new System.Windows.Forms.Padding(2);
             this.treeView1.Name = "treeView1";
             treeNode1.Name = "Application";
@@ -61,7 +80,7 @@
             treeNode1,
             treeNode2,
             treeNode3});
-            this.treeView1.Size = new System.Drawing.Size(149, 395);
+            this.treeView1.Size = new System.Drawing.Size(156, 597);
             this.treeView1.TabIndex = 0;
             this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             // 
@@ -76,14 +95,18 @@
             // 
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.No,
-            this.ID,
+            this.EventID,
             this.Message,
-            this.Source});
+            this.Source,
+            this.DateAndTime});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listView1.FullRowSelect = true;
             this.listView1.GridLines = true;
-            this.listView1.Location = new System.Drawing.Point(175, 10);
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(829, 395);
+            this.listView1.Size = new System.Drawing.Size(837, 597);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
             this.listView1.TabIndex = 1;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -93,23 +116,120 @@
             this.No.Text = "No";
             this.No.Width = 57;
             // 
+            // EventID
+            // 
+            this.EventID.Text = "EventID";
+            this.EventID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.EventID.Width = 86;
+            // 
             // Message
             // 
             this.Message.Text = "Message";
             this.Message.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.Message.Width = 498;
-            // 
-            // ID
-            // 
-            this.ID.Text = "ID";
-            this.ID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.ID.Width = 86;
+            this.Message.Width = 377;
             // 
             // Source
             // 
             this.Source.Text = "Source";
             this.Source.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.Source.Width = 178;
+            this.Source.Width = 180;
+            // 
+            // DateAndTime
+            // 
+            this.DateAndTime.Text = "Date and Time";
+            this.DateAndTime.Width = 132;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer2.Location = new System.Drawing.Point(12, 27);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.listView1);
+            this.splitContainer2.Size = new System.Drawing.Size(997, 597);
+            this.splitContainer2.SplitterDistance = 156;
+            this.splitContainer2.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1021, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportToFileToolStripMenuItem,
+            this.refreshToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exportToFileToolStripMenuItem
+            // 
+            this.exportToFileToolStripMenuItem.Name = "exportToFileToolStripMenuItem";
+            this.exportToFileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exportToFileToolStripMenuItem.Text = "Export to file";
+            this.exportToFileToolStripMenuItem.Click += new System.EventHandler(this.exportToFileToolStripMenuItem_Click);
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 627);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1021, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.MergeAction = System.Windows.Forms.MergeAction.Remove;
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(1000, 16);
             // 
             // form1BindingSource
             // 
@@ -119,15 +239,26 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1016, 423);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.treeView1);
+            this.ClientSize = new System.Drawing.Size(1021, 649);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.splitContainer2);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Event Viewer";
             ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -139,9 +270,21 @@
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader No;
-        private System.Windows.Forms.ColumnHeader ID;
+        private System.Windows.Forms.ColumnHeader EventID;
         private System.Windows.Forms.ColumnHeader Message;
         private System.Windows.Forms.ColumnHeader Source;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ColumnHeader DateAndTime;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem exportToFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
     }
 }
 
--- EventViewer/Form1.cs
+++ EventViewer/Form1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Text;
 using System.Windows.Forms;
 
 namespace EventViewer
@@ -30,20 +32,114 @@ namespace EventViewer
             eventLog1.Log = e.Node.Name;
 
             listView1.Items.Clear();
-          
-            for (int i = 0; i < eventLog1.Entries.Count; i++)
+            toolStripProgressBar1.Value = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                this.printEntries((eventLog1.Entries.Count / 10) * i);
+            }
+
+            // FIXME: Print ...
+
+            for (int i = (eventLog1.Entries.Count / 10) * 10; i < eventLog1.Entries.Count; i++)
             {
                 ListViewItem listViewItem = new ListViewItem(i.ToString());
                 listViewItem.SubItems.Add(eventLog1.Entries[i].InstanceId.ToString());
                 listViewItem.SubItems.Add(eventLog1.Entries[i].Message);
                 listViewItem.SubItems.Add(eventLog1.Entries[i].Source);
-                listView1.Items.Add(listViewItem);
+                listViewItem.SubItems.Add(eventLog1.Entries[i].TimeGenerated.ToLongDateString());
+                listView1.Items.Insert(0, listViewItem);
             }
         }
 
-        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+        private void printEntries(int startIndex, int times = 10)
         {
-            
+            for (int index = 0; index < eventLog1.Entries.Count / 10; index++)
+            {
+                int i = index + startIndex;
+
+                ListViewItem listViewItem = new ListViewItem(i.ToString());
+                listViewItem.SubItems.Add(eventLog1.Entries[i].InstanceId.ToString());
+                listViewItem.SubItems.Add(eventLog1.Entries[i].Message);
+                listViewItem.SubItems.Add(eventLog1.Entries[i].Source);
+                listViewItem.SubItems.Add(eventLog1.Entries[i].TimeGenerated.ToLongDateString());
+                listView1.Items.Insert(0, listViewItem);
+            }
+
+            toolStripProgressBar1.Increment(10);
         }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var message = new StringBuilder();
+            message.AppendLine("Event Viewer");
+            message.AppendLine("Version: 1.0.0");
+            message.AppendLine("View monitoring and troubleshooting messages from windows and other programs");
+            MessageBox.Show(message.ToString());
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void exportToFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string filename = "";
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.Title = "SaveFileDialog Export2File";
+            sfd.Filter = "Text File (.txt) | *.txt";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                filename = sfd.FileName.ToString();
+                if (filename != "")
+                {
+                    using (StreamWriter sw = new StreamWriter(filename))
+                    {
+                        foreach (ListViewItem item in listView1.Items)
+                        {
+                            sw.WriteLine(
+                                "{0}{1}{2}{3}{4}{5}{6}{7}{8}",
+                                item.SubItems[0].Text,
+                                ",",
+                                item.SubItems[1].Text,
+                                ",",
+                                item.SubItems[2].Text,
+                                ",",
+                                item.SubItems[3].Text,
+                                ",",
+                                item.SubItems[4].Text
+                            );
+                        }
+                    }
+                }
+            }
+        }
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            toolStripProgressBar1.Value = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                this.printEntries((eventLog1.Entries.Count / 10) * i);
+            }
+
+            // FIXME: Print ...
+
+            for (int i = (eventLog1.Entries.Count / 10) * 10; i < eventLog1.Entries.Count; i++)
+            {
+                ListViewItem listViewItem = new ListViewItem(i.ToString());
+                listViewItem.SubItems.Add(eventLog1.Entries[i].InstanceId.ToString());
+                listViewItem.SubItems.Add(eventLog1.Entries[i].Message);
+                listViewItem.SubItems.Add(eventLog1.Entries[i].Source);
+                listViewItem.SubItems.Add(eventLog1.Entries[i].TimeGenerated.ToLongDateString());
+                listView1.Items.Insert(0, listViewItem);
+            }
+        }
+       
     }
 }
